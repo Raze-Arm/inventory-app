6fe4179caf00e7cdde59b0e7490db69f32f214af
razar <razear007@gmail.com>
2021-05-20 14:24:57 -0400

    adding services for customer and supplier and product closes #24

---
+++ src/main/java/raze/spring/inventory/converter/service/SupplierService.java
@@ -0,0 +1,18 @@
+package raze.spring.inventory.converter.service;
+
+import raze.spring.inventory.domain.dto.SupplierDto;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface SupplierService {
+    List<SupplierDto> getSupplierList();
+
+    SupplierDto getSupplier(UUID id);
+
+    UUID saveSupplier(SupplierDto supplierDto);
+
+    void updateSupplier(SupplierDto supplierDto);
+
+    void deleteSupplier(UUID id);
+}
---
+++ src/main/java/raze/spring/inventory/converter/service/impl/SupplierServiceImpl.java
@@ -0,0 +1,75 @@
+package raze.spring.inventory.converter.service.impl;
+
+import org.springframework.stereotype.Service;
+import raze.spring.inventory.converter.SupplierDtoToSupplier;
+import raze.spring.inventory.converter.SupplierToSupplierDto;
+import raze.spring.inventory.converter.service.SupplierService;
+import raze.spring.inventory.domain.Supplier;
+import raze.spring.inventory.domain.dto.SupplierDto;
+import raze.spring.inventory.repository.SupplierRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class SupplierServiceImpl implements SupplierService {
+    private final SupplierRepository supplierRepository;
+    private final SupplierDtoToSupplier supplierDtoToSupplier;
+    private final SupplierToSupplierDto supplierToSupplierDto;
+
+      public SupplierServiceImpl(
+          SupplierRepository supplierRepository,
+          SupplierDtoToSupplier supplierDtoToSupplier,
+          SupplierToSupplierDto supplierToSupplierDto) {
+        this.supplierRepository = supplierRepository;
+        this.supplierDtoToSupplier = supplierDtoToSupplier;
+        this.supplierToSupplierDto = supplierToSupplierDto;
+      }
+
+    @Transactional
+    @Override
+    public List<SupplierDto> getSupplierList() {
+
+        return this.supplierRepository.findAll().stream()
+            .map(this.supplierToSupplierDto::convert)
+            .collect(Collectors.toList());
+    }
+
+    @Transactional
+    @Override
+    public SupplierDto getSupplier(UUID id) {
+
+        return this.supplierToSupplierDto.convert(this.supplierRepository.findById(id).orElseThrow());
+    }
+
+    @Transactional
+    @Override
+    public UUID saveSupplier(SupplierDto supplierDto) {
+
+        final Supplier supplier =
+            this.supplierRepository.save(
+                Objects.requireNonNull(this.supplierDtoToSupplier.convert(supplierDto)));
+        return supplier.getId();
+    }
+
+    @Transactional
+    @Override
+    public void updateSupplier(SupplierDto supplierDto) {
+        if(supplierDto.getId() == null) throw new NoSuchElementException();
+        final Supplier supplierToEdit = this.supplierRepository.findById(supplierDto.getId()).orElseThrow();
+        supplierToEdit.setFirstName(supplierDto.getFirstName());
+        supplierToEdit.setLastName(supplierDto.getLastName());
+        supplierToEdit.setAddress(supplierDto.getAddress());
+
+        this.supplierRepository.save(supplierToEdit);
+    }
+
+    @Override
+    public void deleteSupplier(UUID id) {
+        this.supplierRepository.deleteById(id);
+    }
+}
