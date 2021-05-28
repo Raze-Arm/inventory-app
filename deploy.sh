64aa0625610b7c71d955526c53ffb4db1579dd87
razar <razear007@gmail.com>
2021-05-28 16:57:48 -0400

    fixes expose port

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 docker push razear/mega-electric-app:latest
 docker push razear/mega-electric-app:$SHA
 
-fandogh service deploy --image razear/mega-electric-app --version latest --name mega-electric-app  -d
+fandogh service deploy --image razear/mega-electric-app --version latest --name mega-electric-app -p 8080  -d
