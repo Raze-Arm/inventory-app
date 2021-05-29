46f3d715e7a5e0ff08b2f3a9c1a5227a907482dc
razar <razear007@gmail.com>
2021-05-29 07:27:25 -0400

    fixes travis build

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 docker push razear/mega-electric-app:latest
 docker push razear/mega-electric-app:$SHA
 
-fandogh service deploy --image razear/mega-electric-app --version latest --name mega-electric-app -p 8080  -d
+fandogh service deploy --env SPRING_PROFILES_ACTIVE=prod --env JWT_SECRET_KEY=${JWT_SECRET_KEY} --env JWT_TOKEN_EXPIRE=${JWT_TOKEN_EXPIRE} --env JWT_TOKEN_PREFIX=${JWT_TOKEN_PREFIX}  --image razear/mega-electric-app --version $SHA --name mega-electric-app  -p 8080  -d
