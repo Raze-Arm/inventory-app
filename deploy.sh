531302487ca73e213e8a77e907416f9045220790
razar <razear007@gmail.com>
2021-05-30 08:05:18 -0400

    fixes travis build

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ docker push razear/mega-electric-app:$SHA
 
 fandogh service apply -f app-deployment.yml  \
                  -p SHA=$SHA \
-                 -p SPRING_PROFILES_ACTIVE=$SPRING_PROFILES_ACTIVE \
+                 -p SPRING_PROFILES_ACTIVE=prod \
                  -p JWT_SECRET_KEY=$JWT_SECRET_KEY \
                  -p JWT_TOKEN_EXPIRE=$JWT_TOKEN_EXPIRE -p JWT_TOKEN_PREFIX=$JWT_TOKEN_PREFIX \
-                 -p MYSQL_USERNAME=$MYSQL_USERNAME} -p MYSQL_PASSWORD=$MYSQL_PASSWORD \
+                 -p MYSQL_USERNAME=$MYSQL_USERNAME -p MYSQL_PASSWORD=$MYSQL_PASSWORD \
                  -p MYSQL_HOST=$MYSQL_HOST -p MYSQL_DB_NAME=$MYSQL_DB_NAME
