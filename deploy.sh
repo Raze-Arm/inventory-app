8b705f095a32f3fda550149bd7923ee56bedc306
razar <razear007@gmail.com>
2021-05-30 13:00:11 -0400

    fixes travis build

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,14 @@ docker push razear/mega-electric-app:latest
 docker push razear/mega-electric-app:$SHA
 
 #fandogh service deploy --env SPRING_PROFILES_ACTIVE=prod --env JWT_SECRET_KEY=$JWT_SECRET_KEY --env JWT_TOKEN_EXPIRE=${JWT_TOKEN_EXPIRE} --env JWT_TOKEN_PREFIX=${JWT_TOKEN_PREFIX} --env MYSQL_USERNAME=${MYSQL_USERNAME} --env MYSQL_PASSWORD=${MYSQL_PASSWORD} --env MYSQL_HOST=${MYSQL_HOST} --env MYSQL_DB_NAME=${MYSQL_DB_NAME} --image razear/mega-electric-app --version $SHA --name mega-electric-app  -p 8080  -d
-fandogh  secret create --name SECRET_NAME -t environment-secret -f  spring-profiles-active=prod
-fandogh  secret create --name SECRET_NAME -t environment-secret -f  jwt-secret-key=$JWT_SECRET_KEY
-fandogh  secret create --name SECRET_NAME -t environment-secret -f  jwt-token-expire=$JWT_TOKEN_EXPIRE
-fandogh  secret create --name SECRET_NAME -t environment-secret -f  jwt-token-prefix=$JWT_TOKEN_PREFIX
-fandogh  secret create --name SECRET_NAME -t environment-secret -f  mysql-username=$MYSQL_USERNAME
-fandogh  secret create --name SECRET_NAME -t environment-secret -f  mysql-password=$MYSQL_PASSWORD
-fandogh  secret create --name SECRET_NAME -t environment-secret -f  mysql-host=$MYSQL_HOST
-fandogh  secret create --name SECRET_NAME -t environment-secret -f  mysql-db-name=$MYSQL_DB_NAME
+fandogh  secret create --name spring-profiles-active -t environment-secret -f  spring-profiles-active=prod
+fandogh  secret create --name jwt-secret-key -t environment-secret -f  SECRET_NAME=$JWT_SECRET_KEY
+fandogh  secret create --name jwt-token-expire -t environment-secret -f  SECRET_NAME=$JWT_TOKEN_EXPIRE
+fandogh  secret create --name jwt-token-prefix -t environment-secret -f  SECRET_NAME=$JWT_TOKEN_PREFIX
+fandogh  secret create --name mysql-username -t environment-secret -f  SECRET_NAME=$MYSQL_USERNAME
+fandogh  secret create --name mysql-password -t environment-secret -f  SECRET_NAME=$MYSQL_PASSWORD
+fandogh  secret create --name mysql-host -t environment-secret -f  SECRET_NAME=$MYSQL_HOST
+fandogh  secret create --name mysql-db-name -t environment-secret -f  SECRET_NAME=$MYSQL_DB_NAME
 
 fandogh service apply -f app-deployment.yml  \
                  -p SHA=$SHA
